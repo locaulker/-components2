6e366b60e32968a155fd23215f57e2a1e4321eb5
Lawrence <Caulker>
2020-06-22 18:25:59 -0400

    added child components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import faker from "faker"
 import CommentDetail from "./CommentDetail"
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <ApprovalCard>
+        <div>
+          <h4 style={{ color: "red" }}>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          comment="2nd blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          comment="3rd blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   )
 }
